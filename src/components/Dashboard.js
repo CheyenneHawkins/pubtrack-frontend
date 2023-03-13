c3c1c4623831dfd9caf90b534b2f436dbe7ec4d2
Cheyenne Hawkins <cheyenne.hawkins@gmail.com>
2023-03-12 23:28:40 -0500

    dashboard song list query

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,15 +2,69 @@ import { Box, Typography, useTheme, colors } from "@mui/material";
 import MusicPlayer from "./MusicPlayer";
 import SessionFooterBar from "./SessionFooterBar";
 import '../styles/App.scss';
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { login } from "./Login";
+import { gql } from 'graphql-tag'
+
 import { AuthContext } from "../context/authContext";
+import { useQuery } from "@apollo/client";
+
+const GET_USER_DOCS = gql`
+    query GetDocumentsByOwner(
+        $owner: String
+        ) {
+            getDocumentsByOwner(
+                owner: $owner
+                ) {
+                    title
+                    }
+        }
+`
 
 
 export default function Dashboard(){
 
     const { user, logout } = useContext(AuthContext)
+    const [userDocs, setUserDocs] = useState()
 
+    function GetUserDocs(){
+
+        const { loading, error, data } = useQuery(GET_USER_DOCS, {
+            variables: { owner: "cheyenne.hawkins@gmail.com" },
+          });
+        
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
+
+        const docList = data.getDocumentsByOwner.map(doc => (
+            <li key={doc.title}>
+              {doc.title}
+            </li>
+          ))
+
+          
+          return (
+            data !== null &&
+            setUserDocs(docList)
+        );
+
+    }
+
+    // GetUserDocs()
+
+    const { loading, error, data } = useQuery(GET_USER_DOCS, {
+        variables: { owner: user?.email },
+      });
+    
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    const docList = data.getDocumentsByOwner.map(doc => (
+        <li key={doc.title}>
+          {doc.title}
+        </li>
+      ))
+    
     return (
         <>
         <Box 
@@ -32,6 +86,13 @@ export default function Dashboard(){
                 : <h2> You should login. </h2>}
                 <button onClick={()=> {console.log(user)}}>USER</button>
             </Box>
+            {user ?
+                <ul>
+                    {docList}
+                </ul>
+                : "Maybe if you logged in, you'd see your songs!"
+            }
+
             <Box 
                 width='30%' 
                 m='0px 0px 0px 0px' 
